53832ba882ac1e5492e2de1579922b4d56a3b1e8
Pallav Regmi <regmipallav@gmail.com>
2025-06-01 19:09:03 -0500

    adding new nav approach with layout use, need to modify to match the navitems we have

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Navigation from "@/components/navigation";
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navigation />
         {children}
         <SpeedInsights />
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Footer from "@/components/Footer";
 import Hero from "@/components/home_components/Hero";
 import Home_Approach from "@/components/home_components/Home_Approach";
-import Navbar from "@/components/Navbar";
+// import Nav from "@/components/nav";
+// import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
+      {/* <Nav/> */}
+      {/* <Navbar /> */}
       <Hero />
       <Home_Approach />
       <Footer/>
